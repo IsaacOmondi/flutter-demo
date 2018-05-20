f362696ee16ec039112285923df615833032c003
Isaac Omondi <iomondi90@gmail.com>
2018-05-20 21:10:59 +0300

    initial commit

---
+++ lib/views/detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Detail Page"),
+        ),
+        body: new Center(
+          child: new Text("This is the detail page"),
+        ),
+      );
+    }
+}
